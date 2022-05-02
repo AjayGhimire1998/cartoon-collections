10f00ca7842cbb3313faa27be60b3f6cd3976749
AjayGhimire <ajayghimire1998@gmail.com>
2022-05-02 20:49:11 +1000

    commit tree 7bf69a5e45722317c89c756dfa595c7e50703dbd

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves dwarves
   # Your code here
+  dwarves.each.with_index(1) do |dwarf, i|
+    puts "#{i}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet calls# code an argument here
   # Your code here
+  calls.collect {|call| p "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls planteries
+  # code an argument here
   # Your code here
+  planteries.any? {|plantery| plantery.length >4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese snacks# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find {|snack| cheese_types.include?(snack)}
+
 end
